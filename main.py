38e30ccf9036bf97ee839445802e9fa23fc4bad2
M00n-man92 <azraelnumb00@gmail.com>
2021-11-23 13:32:09 +0300

    loop added

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from encript import cesar_encript
 from encript import cesar_decrypt
-answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
-if answer=="en":
-    letter=input("please enter the lettert u wanna cypher   ").lower()
-    shift=int(input("please enter the the number of shift u wanna associate with it   "))
-    cesar_encript(msg=letter,shift=shift)
-    another_answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
-    if another_answer=="de":
-        letter=input("please enter the lettert u wanna decypher   ").lower()
-        shift=int(input("please enter the the number of shift u wanna associate with it   "))
-        cesar_decrypt(msg=letter,shift=shift)
-    elif another_answer=="en":
+yes=True
+while yes:
+    answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
+    if answer=="en":
         letter=input("please enter the lettert u wanna cypher   ").lower()
         shift=int(input("please enter the the number of shift u wanna associate with it   "))
         cesar_encript(msg=letter,shift=shift)
+        another_answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
+        if another_answer=="de":
+            letter=input("please enter the lettert u wanna decypher   ").lower()
+            shift=int(input("please enter the the number of shift u wanna associate with it   "))
+            cesar_decrypt(msg=letter,shift=shift)
+        elif another_answer=="en":
+            letter=input("please enter the lettert u wanna cypher   ").lower()
+            shift=int(input("please enter the the number of shift u wanna associate with it   "))
+            cesar_encript(msg=letter,shift=shift)
+        else:
+            print("thankin you for choosing us")
+    elif answer=="de":
+        letter=input("please enter the lettert u wanna decypher   ").lower()
+        shift=int(input("please enter the the number of shift u wanna associate with it   "))
+        cesar_decrypt(msg=letter,shift=shift)
+        another_answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
+        if another_answer=="de":
+            letter=input("please enter the lettert u wanna decypher   ").lower()
+            shift=int(input("please enter the the number of shift u wanna associate with it   "))
+            cesar_decrypt(msg=letter,shift=shift)
+        elif another_answer=="en":
+            letter=input("please enter the lettert u wanna cypher   ").lower()
+            shift=int(input("please enter the the number of shift u wanna associate with it   "))
+            cesar_encript(msg=letter,shift=shift)
+        else:
+            print("thankin you for choosing us")    
     else:
         print("thankin you for choosing us")
-elif answer=="de":
-    letter=input("please enter the lettert u wanna decypher   ").lower()
-    shift=int(input("please enter the the number of shift u wanna associate with it   "))
-    cesar_decrypt(msg=letter,shift=shift)
-    another_answer=input("input de for decode or en for encrypt or anything else to stop the code ").lower()
-    if another_answer=="de":
-        letter=input("please enter the lettert u wanna decypher   ").lower()
-        shift=int(input("please enter the the number of shift u wanna associate with it   "))
-        cesar_decrypt(msg=letter,shift=shift)
-    elif another_answer=="en":
-        letter=input("please enter the lettert u wanna cypher   ").lower()
-        shift=int(input("please enter the the number of shift u wanna associate with it   "))
-        cesar_encript(msg=letter,shift=shift)
-    else:
-        print("thankin you for choosing us")    
-else:
-    print("thankin you for choosing us")            
+    
+    no=input("do you wanna go agian? press yes or no if you want to quit ")
+    if no=="no":
+        yes=False            
 
 
 
